6fc5e99e0a83bde3687968d40962e048befb8503
Vybekid <felobitto@gmail.com>
2025-07-22 20:27:49 +0300

    se

--- calc.py
+++ calc.py
@@ -1,1 +1,3 @@
-while True: print(eval(input(">>>")))
+Chant = " Hare Krishna\n Hare Krishna\n Krishna Krishna\n Hare Hare\n Hare Rama\n Hare Rama\n Rama Rama\n Hare Hare\n"
+
+print(f"I love You Krishna", Chant)
